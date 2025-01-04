5bf5d55c2fbfad845a1d2cc40c0fc2b934b767bc
b.maskurov <b.maskurov@uzum.com>
2025-01-05 00:28:26 +0500

    changes

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -15,19 +15,19 @@ const Form = () => {
             subject
         }
         tg.sendData(JSON.stringify(data));
-    }, [])
+    }, [country, street, subject])
 
     useEffect(() => {
         tg.onEvent('mainButtonClicked', onSendData)
         return () => {
             tg.offEvent('mainButtonClicked', onSendData)
         }
-    }, [])
+    }, [onSendData])
 
     useEffect(() => {
         tg.MainButton.setParams({
             text: 'Отправить данные'
-        });
+        })
     }, [])
 
     useEffect(() => {
@@ -36,33 +36,33 @@ const Form = () => {
         } else {
             tg.MainButton.show();
         }
-    }, [country, street]);
+    }, [country, street])
 
     const onChangeCountry = (e) => {
-        setCountry(e.target.value);
+        setCountry(e.target.value)
     }
 
     const onChangeStreet = (e) => {
-        setStreet(e.target.value);
+        setStreet(e.target.value)
     }
 
     const onChangeSubject = (e) => {
-        setSubject(e.target.value);
+        setSubject(e.target.value)
     }
 
     return (
-        <div className={'form'}>
+        <div className={"form"}>
             <h3>Введите ваши данные</h3>
             <input
                 className={'input'}
-                type='text'
+                type="text"
                 placeholder={'Страна'}
                 value={country}
                 onChange={onChangeCountry}
             />
             <input
                 className={'input'}
-                type='text'
+                type="text"
                 placeholder={'Улица'}
                 value={street}
                 onChange={onChangeStreet}
