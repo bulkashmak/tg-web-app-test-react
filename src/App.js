22f4af933871d90eef79b86b2168cd3d71d840c5
b.maskurov <b.maskurov@uzum.com>
2025-01-05 00:36:42 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect} from "react";
 import {useTelegram} from "./hooks/useTelegram";
 import Header from "./components/Header/Header";
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes} from 'react-router-dom'
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-    const {tg} = useTelegram();
+    const {onToggleButton, tg} = useTelegram();
 
     useEffect(() => {
         tg.ready();
     }, [])
 
     return (
-        <div className='App'>
+        <div className="App">
             <Header/>
             <Routes>
                 <Route index element={<ProductList/>}/>
-                <Route path={"/form"} element={<Form/>}/>
+                <Route path={'form'} element={<Form/>}/>
             </Routes>
         </div>
     );
